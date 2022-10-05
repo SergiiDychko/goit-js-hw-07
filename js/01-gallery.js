605a14d5dd3dd07f9e3398628eaafca89ddbc622
Sergii Dychko <110810294+SergiiDychko@users.noreply.github.com>
2022-10-05 17:47:34 +0300

    + Task2

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,48 +1,47 @@
+// Імпортуємо масив елементів галереї galleryItems
 import { galleryItems } from './gallery-items.js';
 
-// Створюємо і наповнюємо галерею з масиву galleryItems; 
+// Наповнюємо перелік елементів з масиву galleryItems;
 const galleryRef = document.querySelector('.gallery');
 
 const galleryItemsArr = [];
-galleryItems.forEach(item => {
+galleryItems.forEach(({ original, preview, description }) => {
   galleryItemsArr.push(
     `<div class="gallery__item">
-      <a class="gallery__link" href="${item.original}" target="_self">
+      <a class="gallery__link" href="${original}" target="_self">
         <img
           class="gallery__image"
-          src="${item.preview}"
-          data-source="${item.original}"
-          alt="${item.description}"
+          src="${preview}"
+          data-source="${original}"
+          alt="${description}"
         />
       </a>
     </div>`
   );
 });
-
 galleryRef.insertAdjacentHTML('afterbegin', galleryItemsArr.join(''));
-// ==================================================================
 
 // створення і відкриття модального вікна зі збільшеним фото
 let instance;
 const instanceOpen = event => {
-    event.preventDefault();
-    instance = basicLightbox.create(
-        `
+  event.preventDefault();
+  instance = basicLightbox.create(
+    `
             <img width="800" height="600" src="${event.target.dataset.source}">
         `
-        );
-        if (event.target === event.currentTarget) {
-        return;
-    }
-    instance.show();
-    galleryRef.addEventListener('keyup', instanceClose);
+  );
+  if (event.target === event.currentTarget) {
+    return;
+  }
+  instance.show();
+  galleryRef.addEventListener('keydown', instanceClose);
 };
 
 // закриття модального вікна кнопкою Esc;
 const instanceClose = event => {
   if (event.code === 'Escape') {
     instance.close();
-}
+  }
 };
 
 // оголошуємо слухач кліків по галереї
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,20 @@
+// Імпортуємо масив елементів галереї galleryItems
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
-console.log(galleryItems);
+// Наповнюємо перелік елементів з масиву galleryItems;
+const galleryRef = document.querySelector('.gallery');
+
+const galleryItemsArr = galleryItems.map(
+  ({ original, preview, description }) =>
+    `<a class="gallery__item" href="${original}">
+      <img class="gallery__image" src="${preview}" alt="${description}" />
+    </a>`
+);
+galleryRef.insertAdjacentHTML('afterbegin', galleryItemsArr.join(''));
+
+// створюємо галерею SimpleLightbox
+const lightbox = new SimpleLightbox('.gallery a', {
+//  додаємо опис (alt) внизу фото 
+  captionsData: 'alt',
+  captionDelay: 250,
+});
